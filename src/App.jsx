26424e725f3e8060c8cac01c4ba7f1e89669126c
Nusantara-Lobster <yantov3ah@gmail.com>
2025-04-21 14:45:12 +0700

    Praktikum Pertemuan 9

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Greeting from './components/Greeting';
+function App() {
+ return (
+ <div>
+ <h1>React Frontend - Pertemuan 9</h1>
+ <Greeting name="Mahasiswa" />
+ </div>
+ );
+}
+export default App;
