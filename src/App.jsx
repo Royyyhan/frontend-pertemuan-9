0bd5cb06a371d94fcf545c09e5fb0996ab7ee345
Nusantara-Lobster <yantov3ah@gmail.com>
2025-04-21 15:29:17 +0700

    Praktikum Pertemuan 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Greeting from './components/Greeting';
-function App() {
- return (
- <div>
- <h1>React Frontend - Pertemuan 9</h1>
- <Greeting name="Mahasiswa" />
- </div>
- );
+import Greeting from './components/Greeting'; 
+import ProductList from './components/ProductList';
+import Mahasiswa from './components/Mahasiswa';
+import PostList from './components/PostList';
+
+
+
+function App() { 
+  return ( 
+    <div> 
+      <h1>React Frontend - Pertemuan 9</h1> 
+      <Greeting name="Mahasiswa" /> 
+      <ProductList /> 
+      <Mahasiswa nama="Firmansyah Shurdi" npm="3124522007" prodi="Teknik Informatika" /> 
+      <PostList />
+    </div> 
+  ); 
 }
+ 
 export default App;
